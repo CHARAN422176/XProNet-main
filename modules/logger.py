1fd25f47bc98ed9bae61375f61d797d7e43d5d96
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-06-24 23:33:00 +0530

    commit loogger update

--- modules/logger.py
+++ modules/logger.py
@@ -24,8 +24,14 @@ def create_logger(output_dir, dist_rank=0, name=''):
     color_fmt = colored('[%(asctime)s %(name)s]', 'green') + \
                 colored('(%(filename)s %(lineno)d)', 'yellow') + ': %(levelname)s %(message)s'
 
-    # create console handlers for master process
-    if dist.get_rank() == dist_rank:
+    # # create console handlers for master process
+    # if dist.get_rank() == dist_rank:
+    #     console_handler = logging.StreamHandler(sys.stdout)
+    #     console_handler.setLevel(logging.DEBUG)
+    #     console_handler.setFormatter(
+    #         logging.Formatter(fmt=color_fmt, datefmt='%Y-%m-%d %H:%M:%S'))
+    #     logger.addHandler(console_handler)
+    if not dist.is_available() or not dist.is_initialized() or dist.get_rank() == dist_rank:
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setLevel(logging.DEBUG)
         console_handler.setFormatter(
